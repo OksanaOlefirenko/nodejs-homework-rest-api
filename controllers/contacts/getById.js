a6c78cc59cb4f9bd10ca461ef0cb6c1c10df5511
OksanaOlefirenko <oksana_olefirenko@ukr.net>
2022-10-03 15:21:10 +0300

    add MongoDB

--- controllers/contacts/getById.js
+++ controllers/contacts/getById.js
@@ -1,9 +1,9 @@
-const contactsOperations = require('../../models/contacts');
+const { Contact } = require('../../models/contact');
 const { RequestError } = require('../../helpers');
 
 const getById = async (req, res) => {
   const { contactId } = req.params;
-  const result = await contactsOperations.getContactById(contactId);
+  const result = await Contact.findById(contactId);
   if (!result) {
     throw RequestError(404, `Contact with id =${contactId} was not found`);
   }
